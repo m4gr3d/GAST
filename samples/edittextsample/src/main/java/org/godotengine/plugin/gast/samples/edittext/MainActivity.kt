a72e86c3b5669d5de2397faf304e332528a1e269
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2021-03-08 08:57:05 -0800

    Move the Android and Godot `edittextsample` projects into the same directory.
    This is done by creating a new asset directory for the Godot project files under `src/main/godot`.
    In addition, we update the gradle `ignoreAssetsPattern` to ignore the `android` build directory created by the Godot editor during export.

--- samples/edittextsample/src/main/java/org/godotengine/plugin/gast/samples/edittext/MainActivity.kt
+++ samples/edittextsample/src/main/java/org/godotengine/plugin/gast/samples/edittext/MainActivity.kt
@@ -66,8 +66,12 @@ class MainActivity : Godot() {
     // Override the engine starting parameters to indicate we want VR mode.
     override fun getCommandLine(): Array<String> {
         val original = super.getCommandLine()
-        val updated: Array<String> = Arrays.copyOf(original, original.size + 1)
-        updated[original.size] = XRMode.OVR.cmdLineArg
-        return updated
+        if (BuildConfig.FLAVOR == "ovr") {
+            val updated: Array<String> = Arrays.copyOf(original, original.size + 1)
+            updated[original.size] = XRMode.OVR.cmdLineArg
+            return updated
+        } else {
+            return original
+        }
     }
 }
