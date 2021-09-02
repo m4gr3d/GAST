3897c4619c986fbc280a46499e69174ed0bfe5e9
Dishaan Ahuja <dishaan@google.com>
2021-09-02 11:01:15 -0700

    Update documentation for CustomProjectionMesh

--- core/src/main/java/org/godotengine/plugin/gast/projectionmesh/CustomProjectionMesh.kt
+++ core/src/main/java/org/godotengine/plugin/gast/projectionmesh/CustomProjectionMesh.kt
@@ -2,7 +2,22 @@ package org.godotengine.plugin.gast.projectionmesh
 
 class CustomProjectionMesh(meshPointer: Long, nodePointer : Long) : ProjectionMesh(meshPointer, nodePointer) {
 
-    // TODO: Add docs for this method.
+    /**
+     * Set provided mesh as the current projection mesh.
+     *
+     * If either verticesRight, textureCoordsRight, or glDrawModeRight are null, the left eye mesh
+     * will be used for the right eye as well.
+     *
+     * @param vertices Left eye vertex positions in 3D space [x1, y1, z1 ... xn, yn, zn]
+     * @param textureCoords Left eye texture sampling coords [u1, v1, u2, v2 ... un, vn]
+     * @param glDrawMode Left eye GL mesh primitive
+     * @param verticesRight Right eye vertex positions in 3D space [x1, y1, z1 ... xn, yn, zn]
+     * @param textureCoordsRight Right eye texture sampling coords [u1, v1, u2, v2 ... un, vn]
+     * @param glDrawModeRight Right eye GL mesh primitive
+     * @param stereoMode int representation of stereo mode, matches the
+     * com.google.android.exoplayer2.C.StereoMode constants
+     * @param uvOriginIsBottomLeft Whether the origin in mesh space is bottom left or top left
+     */
     fun setCustomMesh(vertices: FloatArray,
                       textureCoords: FloatArray,
                       glDrawMode: Int,
